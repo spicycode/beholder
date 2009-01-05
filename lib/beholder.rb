d1d944b9d392abdd2e711827c626a144a824ffc2
Chad Humphries <chad@spicycode.com>
2009-01-05 09:09:48 -0500

    Updates

--- lib/beholder.rb
+++ lib/beholder.rb
@@ -40,7 +40,6 @@ class Beholder
   end
   
   def identify_stolen_treasure(treasure)
-    say "Encountered file: #{treasure}"
     case treasure
     when /#{working_directory}\/app\/(.*)\.rb/
       "examples/#{$1}_example.rb"
