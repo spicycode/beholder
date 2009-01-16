bd7d66e20971830505305c0ccc501470da462ed1
Chad Humphries <chad@spicycode.com>
2009-01-16 14:01:27 -0500

    0.5.3 - Burn baby burn!

--- lib/beholder.rb
+++ lib/beholder.rb
@@ -61,18 +61,19 @@ class Beholder
     map_for(:default_dungeon) do |wizard|
       
       wizard.keep_a_watchful_eye_for 'lib', 'examples'
-     
-      wizard.prepare_spell_for /\/lib\/(.*)\.rb/ do |spell_component|
-        ["examples/lib/#{spell_component[1]}_example.rb"]
-      end
-      
-      wizard.prepare_spell_for /\/examples\/(.*)_example\.rb/ do |spell_component|
+
+      wizard.prepare_spell_for %r%examples/(.*)_example\.rb% do |spell_component|
         ["examples/#{spell_component[1]}_example.rb"]
       end
       
-      wizard.prepare_spell_for /\/examples\/example_helper\.rb/ do |spell_component|
+      wizard.prepare_spell_for %r%examples/example_helper\.rb% do |spell_component|
         Dir["examples/**/*_example.rb"]
       end
+     
+      wizard.prepare_spell_for %r%lib/(.*)\.rb% do |spell_component|
+        ["examples/lib/#{spell_component[1]}_example.rb"]
+      end
+      
 
     end
     
